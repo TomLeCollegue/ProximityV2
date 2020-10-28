b778de26b1779da5d87ba6850e14f0ff2ac984cd
TomLeCollegue <tomkubasik.pro@gmail.com>
2020-10-28 19:43:24 +0100

    get id from mail and mdp

--- ProximityApp/app/src/main/java/com/entreprisecorp/proximityv2/MainActivity.java
+++ ProximityApp/app/src/main/java/com/entreprisecorp/proximityv2/MainActivity.java
@@ -3,9 +3,20 @@ package com.entreprisecorp.proximityv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 passwordText = password.getText().toString();
                 mailText = mail.getText().toString();
 
-                Connection(mailText,passwordText);
+                postData();
 
             }
         });
@@ -46,11 +57,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void postData() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
 
-    public void Connection(String mail, String password){
+        String URL = "http://192.168.43.36:8080/RestFullTEST-1.0-SNAPSHOT/account/signIn";
 
-        String URL = "";
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("mail", mail.getText());
+            jsonBody.put("password", password.getText());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
+
+
+        Log.d("test", jsonBody.toString());
+        // Enter the correct url for your api service site
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL, jsonBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            mail.setText("String Response : "+ response.getString("id"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                mail.setText("Error getting response");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
+
 }
