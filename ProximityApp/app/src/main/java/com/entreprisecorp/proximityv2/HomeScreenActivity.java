1af574e740c81f2f77131b62f89c5085ce745d9e
TomLeCollegue <tomkubasik.pro@gmail.com>
2020-10-28 17:02:18 +0100

    homeScreen design

---
+++ ProximityApp/app/src/main/java/com/entreprisecorp/proximityv2/HomeScreenActivity.java
@@ -0,0 +1,14 @@
+package com.entreprisecorp.proximityv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
--- ProximityApp/app/src/main/java/com/entreprisecorp/proximityv2/MainActivity.java
+++ ProximityApp/app/src/main/java/com/entreprisecorp/proximityv2/MainActivity.java
@@ -3,12 +3,54 @@ package com.entreprisecorp.proximityv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mail;
+    private TextView password;
+    private Button connectionButton;
+
+    private String mailText;
+    private String passwordText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //----------link with view-----------------------------//
+        mail = findViewById(R.id.mail);
+        password = findViewById(R.id.password);
+        connectionButton = findViewById(R.id.buttonconnection);
+
+
+        //----------Listener Button----------------------------//
+        connectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passwordText = password.getText().toString();
+                mailText = mail.getText().toString();
+
+                Connection(mailText,passwordText);
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+    public void Connection(String mail, String password){
+
+        String URL = "";
+
+
     }
 }
