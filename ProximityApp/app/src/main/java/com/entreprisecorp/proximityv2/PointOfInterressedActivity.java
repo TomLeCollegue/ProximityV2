e12c0a26d4f99abf17c79719bccea60df0a60e2c
Kubasik <tomkubasik.pro@gmail.com>
2020-11-02 17:00:20 +0100

    started hobby activity

---
+++ ProximityApp/app/src/main/java/com/entreprisecorp/proximityv2/PointOfInterressedActivity.java
@@ -0,0 +1,16 @@
+package com.entreprisecorp.proximityv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PointOfInterressedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point_of_interressed);
+
+
+    }
+}
